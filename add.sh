36d76393242d32e079b1738918440ab7e8c50442
Cristian Ariza <dev@cristianrz.com>
2020-02-15 18:36:47 +0000

    Code golf

--- add.sh
+++ add.sh
@@ -10,28 +10,17 @@ fi
 n="$1" && shift
 text="$1" && shift
 
-if test "$#" -eq 0; then
-	# Input comes from stdin
-	swp="$(cat)"
-else
-	# Input comes from filename
-	swp="$(cat "$1")" && shift
-fi
+swp="$(cat "$@")"
+
+# If swp is not empty, add a newline to it
+swp="${swp:+"$swp"\\n}"
 
 case "$n" in
-"-a")
-	if test -z "$swp"; then
-		printf '%s\n' "$text"
-	else
-		printf '%s\n%s\n' "$swp" "$text"
-	fi
-	;;
-0)
-	printf '%s\n%s\n' "$text" "$swp"
-	;;
+"-a") printf '%s%s\n' "$swp" "$text" ;;
+0) printf '%s\n%s' "$text" "$swp" ;;
 *)
-	printf '%s\n' "$swp" | sed -n 1,"$n"p
+	printf '%s' "$swp" | sed -n 1,"$n"p
 	printf '%s\n' "$text"
-	printf '%s\n' "$swp" | sed -n "$((n + 1))",\$p
+	printf '%s' "$swp" | sed -n "$((n + 1))",\$p
 	;;
 esac
